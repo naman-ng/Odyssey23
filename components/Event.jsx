450ebee5b3d1611554a2585178a6e67f214df6d7
ananyayay <ananyaya2002@gmail.com>
2023-01-01 04:18:36 +0530

    event expand working

--- components/Event.jsx
+++ components/Event.jsx
@@ -1,33 +1,69 @@
+import { transform } from 'framer-motion'
 import React,{useState} from 'react'
-import Image from 'next/image';
 
-function Event({name, tagline, knowmore, bg, image, align, height, key}) {
-  const alignVal = (key+1)%2==0?"left":"right"
+function Event({name, tagline, knowmore, bg, image, align, key}) {
+
+  let alignVal = align%2===0?"left":"right"
   const[isExpanded,setExpanded] = useState(false)
-  console.log(name)
-  return (
-    <div className=" row w-screen h-80"  style={{backgroundColor: bg, border: '2px solid #FFC809 '} } >
-          <img className={`ml-10 mr-20 h-80 float-${alignVal}`} src= {image}  alt="Logo"  style={{
-                  float: alignVal,
-          }} />
+  let buttonText= "Know More"
+  let height= 300
+  let heightImg= 250
 
-          <div className="ml-20 mt-20"  >
-            <div className="col-2 text-6xl mt-9">
-                  {name}
-            </div>
-            <div className="mt-1 col-2 text-3xl  ">
-            {tagline}
-            </div>
-                <div className="btn-group"
-                    role="group"
-                    aria-label="Basic mixed styles example"
-                    style={{color: bg }}>
-                    <button className="font-bold text-4xs mt-4 bg-[#C18C6C] rounded-lg p-2"
-                            type="button"
-                            onClick={() => setExpanded(prev=>!prev)}>
-                      Know More
-                    </button>  
-                </div>
+  function setButtonText(prev){
+    buttonText= prev == true? "Show Less":"Know More";
+  }
+  function setHeight(prev){
+    height= prev == true? 350:300;
+  }
+
+  return (
+    <div  className= { ` w-screen transition-timing-function-[ease-in] 
+                          ${(isExpanded)
+                              ? "translate-y-[0.1vw] duration-[1000ms]"
+                              : "translate-y-[0.1vw] duration-[1000ms]"
+                            } transform transition-all `}
+          style={{backgroundColor: bg, border: '2px solid #FFC809 ',  height: (isExpanded)? 360 :height     }} >  
+      <img className={`mt-5   ${(isExpanded)
+                  ? "translate-y-[0.1vw] duration-[1000ms]"
+                  : "translate-y-[0.1vw] duration-[1000ms]"
+                  } transform transition-all    
+              ${ (alignVal==="left") ? "ml-20 mr-10" : "mr-40" } `}
+            src= {image}  alt="image"  style={{
+                                          float: alignVal,
+                                          height: (isExpanded)? 300 :heightImg   }} />
+
+      <div className={`ml-20 mt-20 ${(isExpanded) 
+                                    ? "" : "translate-y-[0.1vw] duration-[1000ms]" }`}>
+        <div className= {`translate-y-[0.1 vw] duration-[1200ms]
+        ${(isExpanded)
+            ? "text-6xl mt-9" : "text-3xl mt-10" } `}>
+          {name} </div>
+
+        <div className=
+        {` translate-y-[0.1vw] duration-[1000ms] ${(isExpanded)
+                                              ? "text-2xl " : "text-1xl " }`} >
+          {tagline}
+        </div>
+
+        <div className= "font-italic mt-1 mr-40 col-2 text-4xs transition-delay-[10s] translate-y-[1vw] duration-[100ms] " >
+        {(isExpanded)? knowmore:""} 
+        </div>
+      
+          <div className="btn" style={{ color: bg }}>
+              <button className={` font-bold text-[2xs] mt-4 bg-[#F9F9F9] rounded-lg p-1 
+                      ${(isExpanded)
+                        ? "translate-y-[1vw] duration-[1000ms]"
+                        : "translate-y-[-1vw] duration-[1000ms]"
+                      }  transform transition-all
+                      ${(isExpanded)
+                        ? "text-2xs " : "text-xs "} ` } 
+                        type="button"
+                        onClick= { () => { setExpanded(prev => !prev)}}  >
+                      {setButtonText(isExpanded)}
+                      {setHeight(isExpanded)}
+                      {buttonText}
+              </button>  
+          </div>
           </div>    
     </div>
   )
--- pages/Events.jsx
+++ pages/Events.jsx
@@ -7,47 +7,42 @@ function Events() {
         name: "Fashion",
         tagline: "Join the fashion elite at our exclusive fest events.",
         knowmore: "Details of events of events of events of events of events of events of events of events of events of events of events of events of events.",
-        bg: "#7A1501",
+        bg: "#B8215A",
         image: "/images/fashion.jpg",
-        align: 1,
-        height: 350
-
+        align:0
     },
     {
         name: "Quizzing",
         tagline: "Are you ready to face off against other trivia enthusiasts? Join us!",
         knowmore: "Details of events of events of events of events of events of events of events of events of events of events of events of events of events.",
-        bg: "#00381F",
+        bg: "#EE2D69",
         image: "/images/quizzing.jpg",
-        align: 0,
-        height: 350
-
+        align:1
     },
     {
         name: "Dance",
         tagline: "Experience the thrill of the dance floor at our fest event.",
         knowmore: "Details of events of events of events of events of events of events of events of events of events of events of events of events of events of events of events of events.",
-        bg: "#121D21",
+        bg: "#00B9AD",
         image: "/images/dance.jpg",
-        align: 1,
-        height: 350
-
+        align:0
     }
   ]
-  
-
   return (
   
-    <div className="z-0 font-mulish flex flex-col text-xl pt-[5vmax] overflow-clip  text-[#C18C6C] " >
+    <div className="z-0 font-mulish flex flex-col text-xl pt-[5vmax] overflow-clip  text-[#F9F9F9] " >
       {eventList.map((event, i)=>(
+        <main>
              <Event name={event.name}
                     tagline={event.tagline}
                     knowmore={event.knowmore}
                     bg={event.bg}
                     image={event.image}
                     align={event.align}
-                    height ={event.height}
                     key={i}/>
+              <img className='w-screen mb-[-0.1vw] mt-[0.1vw]' src="/images/tealborder.png" 
+                    style={{  width: "100%" }}  />
+        </main>
       ))}
     </div>
     
