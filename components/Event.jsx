63b6efc5f4255018c98370e69889a0f2dc7be8d7
mishraanis <mishraanis@gmail.com>
2023-01-02 15:14:16 +0530

    responsiveness done

--- components/Event.jsx
+++ components/Event.jsx
@@ -1,72 +1,94 @@
-import { transform } from 'framer-motion'
-import React,{useState} from 'react'
+import { transform } from "framer-motion";
+import React, { useState } from "react";
 
-function Event({name, tagline, knowmore, bg, image, align, key}) {
+function Event({ name, tagline, knowmore, bg, image, align, key }) {
+  let alignVal = align % 2 === 0 ? "left" : "right";
+  const [isExpanded, setExpanded] = useState(false);
+  let buttonText = "Know More";
+  let height = 300;
+  let heightImg = 250;
 
-  let alignVal = align%2===0?"left":"right"
-  const[isExpanded,setExpanded] = useState(false)
-  let buttonText= "Know More"
-  let height= 300
-  let heightImg= 250
-
-  function setButtonText(prev){
-    buttonText= prev == true? "Show Less":"Know More";
+  function setButtonText(prev) {
+    buttonText = prev == true ? "Show Less" : "Know More";
   }
-  function setHeight(prev){
-    height= prev == true? 350:300;
+  function setHeight(prev) {
+    height = prev == true ? 350 : 300;
   }
 
   return (
-    <div  className= { ` w-screen transition-timing-function-[ease-in] 
-                          ${(isExpanded)
+    <div
+      className={` w-full ${
+                            isExpanded
                               ? "translate-y-[0.1vw] duration-[1000ms]"
                               : "translate-y-[0.1vw] duration-[1000ms]"
-                            } transform transition-all `}
-          style={{backgroundColor: bg, border: '2px solid #FFC809 ',  height: (isExpanded)? 360 :height     }} >  
-      <img className={`mt-5   ${(isExpanded)
-                  ? "translate-y-[0.1vw] duration-[1000ms]"
-                  : "translate-y-[0.1vw] duration-[1000ms]"
-                  } transform transition-all    
-              ${ (alignVal==="left") ? "ml-20 mr-10" : "mr-40" } `}
-            src= {image}  alt="image"  style={{
-                                          float: alignVal,
-                                          height: (isExpanded)? 300 :heightImg   }} />
+                          } transform transition-all `}
+      style={{
+        backgroundColor: bg,
+        border: "2px solid #FFC809 ",
+        height: isExpanded ? 360 : height,
+      }}
+    >
+      <img
+        className={`mt-5   ${
+          isExpanded
+            ? "translate-y-[0.1vw] duration-[1000ms]"
+            : "translate-y-[0.1vw] duration-[1000ms]"
+        } transform transition-all    
+              ${alignVal === "left" ? "ml-20 mr-10" : "mr-40"} `}
+        src={image}
+        alt="image"
+        style={{
+          float: alignVal,
+          height: isExpanded ? 300 : heightImg,
+        }}
+      />
 
-      <div className={`ml-20 mt-20 ${(isExpanded) 
-                                    ? "" : "translate-y-[0.1vw] duration-[1000ms]" }`}>
-        <div className= {`translate-y-[0.1 vw] duration-[1200ms]
-        ${(isExpanded)
-            ? "text-6xl mt-9" : "text-3xl mt-10" } `}>
-          {name} </div>
+      <div
+        className={`ml-20 mt-20 ${
+          isExpanded ? "" : "translate-y-[0.1vw] duration-[1000ms]"
+        }`}
+      >
+        <div
+          className={`translate-y-[0.1 vw] duration-[1200ms]
+        ${isExpanded ? "text-6xl mt-9" : "text-3xl mt-10"} `}
+        >
+          {name}{" "}
+        </div>
 
-        <div className=
-        {` translate-y-[0.1vw] duration-[1000ms] ${(isExpanded)
-                                              ? "text-2xl " : "text-1xl " }`} >
+        <div
+          className={` translate-y-[0.1vw] duration-[1000ms] ${
+            isExpanded ? "text-2xl " : "text-1xl "
+          }`}
+        >
           {tagline}
         </div>
 
-        <div className= "font-italic mt-1 mr-40 col-2 text-4xs transition-delay-[10s] translate-y-[1vw] duration-[100ms] " >
-        {(isExpanded)? knowmore:""} 
+        <div className="font-italic mt-1 mr-40 col-2 text-4xs transition-delay-[10s] translate-y-[1vw] duration-[100ms] ">
+          {isExpanded ? knowmore : ""}
         </div>
-      
-          <div className="btn" style={{ color: bg }}>
-              <button className={` font-bold text-[2xs] mt-4 bg-[#F9F9F9] rounded-lg p-1 
-                      ${(isExpanded)
-                        ? "translate-y-[1vw] duration-[1000ms]"
-                        : "translate-y-[-1vw] duration-[1000ms]"
+
+        <div className="btn" style={{ color: bg }}>
+          <button
+            className={` font-bold text-[2xs] mt-4 bg-[#F9F9F9] rounded-lg p-1 
+                      ${
+                        isExpanded
+                          ? "translate-y-[1vw] duration-[1000ms]"
+                          : "translate-y-[-1vw] duration-[1000ms]"
                       }  transform transition-all
-                      ${(isExpanded)
-                        ? "text-2xs " : "text-xs "} ` } 
-                        type="button"
-                        onClick= { () => { setExpanded(prev => !prev)}}  >
-                      {setButtonText(isExpanded)}
-                      {setHeight(isExpanded)}
-                      {buttonText}
-              </button>  
-          </div>
-          </div>    
+                      ${isExpanded ? "text-2xs " : "text-xs "} `}
+            type="button"
+            onClick={() => {
+              setExpanded((prev) => !prev);
+            }}
+          >
+            {setButtonText(isExpanded)}
+            {setHeight(isExpanded)}
+            {buttonText}
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Event
+export default Event;
--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -1,48 +1,70 @@
-import React from 'react'
-import Form from '../components/contact/Form';
-import {useTransform,
-    useMotionValue, 
-    motion} from 'framer-motion' 
+import React from "react";
+import Form from "../components/contact/Form";
+import { useTransform, useMotionValue, motion } from "framer-motion";
 
 function ContactMain() {
-	const x = useMotionValue(0);
-	const y = useMotionValue(0);
-	const xposition1 = useTransform(x,[0,2000],[-10,10]);
-	const xposition3 = useTransform(x,[0,2000],[-30,30]);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const xposition1 = useTransform(x, [0, 2000], [-10, 10]);
+  const xposition3 = useTransform(x, [0, 2000], [-30, 30]);
 
-	const yposition1 = useTransform(y,[0,2000],[-10,10]);
-	const yposition3 = useTransform(y, [0, 2000], [-30, 30]);
-	const handleMouse=(e)=>{
-		x.set(e.pageX);
-		y.set(e.pageY);
-		console.log(x,y, "x,y")
-	}
-	return (
-		<div style={{background: "linear-gradient(178.63deg, #1C1E56 3.09%, #8E3A3A 98.83%)"}} 
-			 className='flex w-full md:h-[100vh] h-[70vh] flex-row justify-center align-middle mb-[20vw]' 
-			 onMouseMove={(e)=>handleMouse(e)}>
+  const yposition1 = useTransform(y, [0, 2000], [-10, 10]);
+  const yposition3 = useTransform(y, [0, 2000], [-30, 30]);
+  const handleMouse = (e) => {
+    x.set(e.pageX);
+    y.set(e.pageY);
+  };
+  return (
+    <div
+      style={{
+        background: "linear-gradient(178.63deg, #1C1E56 3.09%, #8E3A3A 60%, #1C1E56 99%)",
+      }}
+      className="relative flex w-[100%] top-40 lg:top-0 h-[100vh] justify-center align-middle overflow-x-clip mb-[20vw] "
+      onMouseMove={(e) => handleMouse(e)}
+    >
+      <motion.div
+        className="absolute z-10 w-full h-[64vmax] -top-24 md:top-0 bg-clouds bg-no-repeat bg-contain "
+        style={{
+          translateX:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? xposition1
+              : 0,
+          translateY:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? yposition1
+              : 0,
+        }}
+      />
+      <motion.div
+        className="absolute z-10 w-[106.5vw] xl:w-[104.5vw] h-[70rem] md:h-[70rem] bg-ground bg-no-repeat bg-contain top-[5rem] md:top-[0rem] -left-[2vw]"
+        style={{
+          translateX:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? xposition3
+              : 0,
+          translateY:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? yposition3
+              : 0,
+        }}
+      />
+      <motion.div
+        className="absolute z-5 w-[100%] h-[20vmax] bg-road bg-no-repeat bg-contain top-[20rem]  md:top-[65vw] xl:top-[63vw] left-[-2vw] "
+        style={{
+          translateX:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? xposition3
+              : 0,
+          translateY:
+            ((typeof window !== "undefined") && (window.innerWidth > 1024))
+              ? yposition3
+              : 0,
+          width: "104vw",
+        }}
+      />
 
-			<motion.div className='absolute z-20 w-full h-[100%] bg-clouds bg-no-repeat bg-cover '
-						style={{
-						translateX:xposition1,
-						translateY:yposition1
-						}}/>
-				
-			<motion.div className='absolute z-20 md:w-[110vmax] md:h-[120%] w-[115%] h-[75%] mb-12 bg-ground bg-no-repeat bg-cover '
-						style={{
-						translateX:xposition3,
-						translateY: yposition3,
-						}}/>
-
-			<motion.div className='md:block hidden absolute z-10 md:w-[110%] h-[20vmax] bg-road bg-no-repeat bg-contain  bottom-[-30vw] left-[-2vw] '
-						style={{
-						translateX: xposition3,
-						translateY: yposition3,
-						}}/>
-			<Form />
-			
-		</div>
-	)
-
+      <Form />
+    </div>
+  );
 }
-export default ContactMain
+export default ContactMain;
