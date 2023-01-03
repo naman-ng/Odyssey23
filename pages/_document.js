aed0b0a0881ef7eb2e4ec8bae6c17467163638e3
naman-ng <naman21171@iiitd.ac.in>
2023-01-03 20:37:18 +0530

    f title

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title> Odyssey</title>{' '}
+      </Head>
       <body>
-        <link href='https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900;1000&display=swap' 
-              rel='stylesheet'/>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet" />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
