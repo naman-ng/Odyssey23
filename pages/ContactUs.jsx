cde16ed7c60e9435f3266b473423697b2b02a243
mishraanis <mishraanis@gmail.com>
2023-01-03 19:32:29 +0530

    responsiveness done

--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "../components/contact/Form";
 import { useTransform, useMotionValue, motion } from "framer-motion";
 
