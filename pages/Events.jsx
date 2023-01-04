ab6cb96fa852811cc8b2c21517d954539532fd45
naman-ng <naman21171@iiitd.ac.in>
2023-01-05 00:58:02 +0530

    add short loader

--- pages/Events.jsx
+++ pages/Events.jsx
@@ -44,7 +44,7 @@ function Events() {
   return loading ? (
     <div className="">
       <video loop autoPlay muted className="h-full w-full object-cover rounded-[1.5vmax]">
-        <source src={'/loader.mp4'} type="video/mp4" />
+        <source src={'/shortLoader.mp4'} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
   return loading ? (
     <div className="">
       <video loop autoPlay muted className="h-full w-full object-cover rounded-[1.5vmax]">
-        <source src={'/loader.mp4'} type="video/mp4" />
+        <source src={'/shortLoader.mp4'} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
