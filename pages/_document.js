fcd99e2ee3c2794849a9198933da7d511d9a601f
naman-ng <naman21171@iiitd.ac.in>
2023-01-06 23:21:31 +0530

    Change title

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title> Odyssey</title>{' '}
+        <title>Odyssey'23 | IIITD</title>{' '}
       </Head>
       <body>
         <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet" />
